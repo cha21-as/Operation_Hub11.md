49e56f838950fbe6ebc345ea3391b02f72519dc4
cha21-as <charelleassaf@gmail.com>
2026-09-15 16:12:05 +0300

    changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'x-actor-role', 'x-actor-department'],
+    credentials: true,
+  });
+
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
